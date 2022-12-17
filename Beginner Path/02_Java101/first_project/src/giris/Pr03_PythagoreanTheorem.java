6c35a018fff6a1a657e7ef586fe59e546b8d64f0
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2022-12-17 23:54:47 +0300

    project 3 completed

---
+++ Beginner Path/02_Java101/first_project/src/giris/Pr03_PythagoreanTheorem.java
@@ -0,0 +1,33 @@
+/**
+ * Program that calculates hypotenuse using Pythagorean Theorem
+ *
+ * @author: Yağmur Yıldız
+ * @date: 17 December 2022
+ */
+
+package giris;
+import java.util.Scanner;
+public class Pr03_PythagoreanTheorem {
+    public static void main(String[] args) {
+
+        //Initiate variables
+        float a,b;
+        double c;
+
+        //Initiate Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Get values
+        System.out.print("Please enter a leg: ");
+        a = input.nextFloat();
+
+        System.out.print("Please enter b leg: ");
+        b = input.nextFloat();
+
+        //Output
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hypotenuse is " + c);
+
+
+    }
+}
