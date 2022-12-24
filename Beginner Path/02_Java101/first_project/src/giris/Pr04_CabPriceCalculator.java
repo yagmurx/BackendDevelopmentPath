66f775c71e3cd3c7375a7adb29ec71fa6b014681
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2022-12-25 01:26:33 +0300

    java examples

---
+++ Beginner Path/02_Java101/first_project/src/giris/Pr04_CabPriceCalculator.java
@@ -0,0 +1,28 @@
+/**
+ * Program that calculate the cab price based on the distance entered.
+ * Opening Fee = $10, price per km = $2.20
+ * @author: Yagmur Yildiz
+ * @date: 25 December 2022
+ */
+
+package giris;
+import java.util.Scanner;
+
+public class Pr04_CabPriceCalculator {
+    public static void main(String[] args) {
+        // Variables
+        float distance = 0 ,price = 0, pricePerKm = 2.20F, openFee = 10F;
+
+        // Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter distance as km: ");
+        distance = input.nextFloat();
+
+        //Calculation
+        price = openFee + (distance*pricePerKm);
+        price = (price < 20) ? 20 : price;
+
+        // Output
+        System.out.println("Price = " + price);
+    }
+}
---
+++ Beginner Path/02_Java101/first_project/src/giris/Pr05_CircleCalculator.java
@@ -0,0 +1,31 @@
+/**
+ * Program that calculate perimeter & area of a circle from radius.
+ *
+ * @author: Yagmur Yildiz
+ *
+ */
+package giris;
+import java.util.Scanner;
+
+public class Pr05_CircleCalculator {
+    public static void main(String[] args) {
+        //Variables
+        float radius=0, pi=3.14F, per=0, area=0;
+
+        //Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the radius: ");
+        radius = input.nextFloat();
+
+        //Calculation
+        per = 2*pi*radius;
+        area = pi*radius*radius;
+
+        //Output
+        System.out.println("Perimeter of the circle: " + per);
+        System.out.println("Are of the circle: " + area);
+
+
+
+    }
+}
