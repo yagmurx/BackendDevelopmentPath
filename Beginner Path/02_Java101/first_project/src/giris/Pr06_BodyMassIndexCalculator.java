186810272aa5576bdfa3aaf010f569b70914c363
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2022-12-25 23:19:42 +0300

    bmi calculator completed

---
+++ Beginner Path/02_Java101/first_project/src/giris/Pr06_BodyMassIndexCalculator.java
@@ -0,0 +1,31 @@
+/**
+ * A program that calculates body mass index by inputs.
+ *
+ * @author: Yagmur Yildiz
+ * @date: 25 December 2022
+ */
+
+package giris;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Pr06_BodyMassIndexCalculator {
+    public static void main(String[] args) {
+        //Variables
+        float height=0, weight=0, bmi=0;
+
+        //Input
+        Scanner in_h = new Scanner(System.in);
+        Scanner in_w = new Scanner(System.in);
+
+        System.out.print("Please enter your height in meter: ");
+        height = in_h.nextFloat();
+        System.out.print("Please enter your weight in meter: ");
+        weight = in_w.nextFloat();
+
+        // Calculation
+        bmi = weight / (height*height);
+
+        //Output
+        System.out.println("Your BMI is " + bmi);
+    }
+}
