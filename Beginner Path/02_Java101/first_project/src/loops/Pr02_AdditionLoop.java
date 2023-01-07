3bb64dca0511334d8ce07ccbe933f74e421495f4
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2023-01-07 20:03:41 +0300

    addition loop completed

---
+++ Beginner Path/02_Java101/first_project/src/loops/Pr02_AdditionLoop.java
@@ -0,0 +1,23 @@
+/**
+ *  A program that summs odd numbers
+ */
+
+
+package loops;
+import java.util.Scanner;
+public class Pr02_AdditionLoop {
+    public static void main(String[] args) {
+        int num, sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a number: ");
+            num = input.nextInt();
+            if(num%2 == 1)
+                sum += num;
+        } while(num > 0);
+
+        System.out.println("Total: " + sum);
+    }
+}
