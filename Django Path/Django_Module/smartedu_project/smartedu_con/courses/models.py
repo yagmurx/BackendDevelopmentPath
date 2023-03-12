f26f3355efab404775429694a2aa889a40bb025e
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2023-03-12 22:38:01 +0300

    categories class created

--- Django Path/Django_Module/smartedu_project/smartedu_con/courses/models.py
+++ Django Path/Django_Module/smartedu_project/smartedu_con/courses/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True, verbose_name="Course Name", help_text="Enter the course name")
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="default_course_image.jpg")
     date = models.DateTimeField(auto_now=True)
@@ -9,4 +17,3 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
-
