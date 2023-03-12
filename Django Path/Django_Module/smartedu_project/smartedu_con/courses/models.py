d7d3261b449b0f2bf1b234c857913add2d272e97
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2023-03-12 17:43:21 +0300

    courses app and objects created

---
+++ Django Path/Django_Module/smartedu_project/smartedu_con/courses/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name="Course Name", help_text="Enter the course name")
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
