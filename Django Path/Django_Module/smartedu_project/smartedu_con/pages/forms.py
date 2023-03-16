624e395517439141aacbb9519aa3371f17324f7b
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2023-03-16 23:46:55 +0300

    contact form completed

--- Django Path/Django_Module/smartedu_project/smartedu_con/pages/forms.py
+++ Django Path/Django_Module/smartedu_project/smartedu_con/pages/forms.py
@@ -2,27 +2,27 @@ from django import forms
 from . models import Contact
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs= {
-        'class' : 'form-control',
-        'placeholder' : 'First Name',
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'First Name'
     }))
-    last_name = forms.CharField(widget=forms.TextInput(attrs= {
-        'class' : 'form-control',
-        'placeholder' : 'Last Name',
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Last Name'
     }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs= {
-        'class' : 'form-control',
-        'placeholder' : 'Email',
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Your Email'
     }))
-    phone = forms.CharField(widget=forms.TextInput(attrs= {
-        'class' : 'form-control',
-        'placeholder' : 'Phone Number',
+    phone = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Your Phone'
     }))
-    message = forms.CharField(widget=forms.Textarea(attrs= {
-        'class' : 'form-control',
-        'placeholder' : 'Your Message',
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Your Message'
     }))
 
-class Meta:
-    model = Contact
-    fields = ['first_name', 'last_name', 'email', 'phone', 'message']
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'email', 'phone', 'message']
